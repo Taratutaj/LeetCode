a54476d7ad03dabf9f9a4a218b52d0bd025acb87
Joanna Taratuta <94808296+Taratutaj@users.noreply.github.com>
2024-02-12 11:44:23 +0100

    Time: 344 ms (5.04%) | Memory: 58.8 MB (5.24%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let n=nums.length;
+    let m=n/2;
+    console.log(m)
+    const counter = {};
+    let max;
+    if(n==1){return nums[0]}
+    else{
+    nums.forEach(ele => {
+    console.log(ele)
+    if (counter[ele]) {
+        counter[ele] += 1;
+        
+        if(counter[ele]>m){
+        max = ele
+        }
+    } else {
+        counter[ele] = 1;
+    }})
+    
+    return max  
+}}
