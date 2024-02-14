b99cf4139f846ea83771954bfe3ef5d5881618a2
Joanna Taratuta <94808296+Taratutaj@users.noreply.github.com>
2024-02-14 13:05:12 +0100

    Time: 374 ms (10.57%) | Memory: 100.1 MB (18.49%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    
+    const positiveArr =[]
+    const negativeArr =[]
+    const result = []
+
+    for(let item of nums){
+        if(item>0){
+            positiveArr.push(item)
+        }else{
+            negativeArr.push(item)
+        }
+    }
+    const n = nums.length/2;
+    let i = 0;
+    while(i<n){
+        result.push(positiveArr[i])
+        result.push(negativeArr[i])
+        i+=1
+    }
+    console.log(positiveArr, negativeArr, result)
+    return result
+};
