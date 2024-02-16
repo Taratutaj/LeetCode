5f2d901945ce6d21b6a6abd90befaff6c55ec4bd
Joanna Taratuta <94808296+Taratutaj@users.noreply.github.com>
2024-02-16 20:27:41 +0100

    Time: 331 ms (10.77%) | Memory: 76.8 MB (59.49%) - LeetSync

---
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findLeastNumOfUniqueInts = function (arr, k) {
+    const frequencySort = (arr = []) => {
+        let map = {};
+        for (let i = 0; i < arr.length; i++) {
+            map[arr[i]] = (map[arr[i]] || 0) + 1;
+        };
+        return arr.sort((a, b) => map[a] - map[b] || b - a);
+    };
+    frequencySort(arr);
+    console.log(arr);
+
+    arr.splice(0, k)
+    console.log(arr);
+
+    let result = 1
+    if (arr.length > 1) {
+        for (let i = 1; i < arr.length; i++) {
+            if(arr[i-1]!==arr[i])
+            result +=1
+        }
+    }
+    else {
+        return arr.length
+    }
+    return result
+
+}
