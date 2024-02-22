fa0eda3ae211846082ebada39944768b176f848c
Joanna Taratuta <94808296+Taratutaj@users.noreply.github.com>
2024-02-22 10:27:37 +0100

    Time: 98 ms (33.50%) | Memory: 57.5 MB (42.00%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.js
+++ 1039-find-the-town-judge/find-the-town-judge.js
@@ -8,27 +8,24 @@ var findJudge = function (n, trust) {
         { length: n },
         (_, index) => index + 1
     );
-    console.log(arr)
     const arrCount = new Array(n);
     arrCount.fill(0);
-console.log(arrCount)
+
 
     for (let person of trust) {
-        
-        console.log("Osoba pierwsza: ", person[0])
-        console.log("Osoba druga: ", person[1])
+
         arrCount[person[1]-1]+=1
         let index = arr.indexOf(person[0]);
         if(index == -1){continue}
         arr.splice(index, 1);
-        console.log("Podejrzani: ", arr)
+        
     }
-    console.log(arrCount)
+
     if (arr.length !== 1) {
-        console.log("za dużo lub za mało podejrzanych: ",arr)
+
         return -1
     }
-    console.log(arr[0])
+
     if(arrCount[arr[0]-1]==n-1){
         return arr[0]
     }else return -1
