676a322dfeae5e05f698d4c4c6a4f80840f4ee9f
Joanna Taratuta <94808296+Taratutaj@users.noreply.github.com>
2024-03-08 13:44:18 +0100

    Time: 174 ms (33.97%) | Memory: 64.9 MB (60.51%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a,b)=>a-b)
+    let n=nums.length
+    let arr=[]
+    let sum = 0
+    
+    
+    for(let i = 0; i<n-2;i++){ 
+        if(i>0 && nums[i] == nums[i-1]){
+            continue;
+        }
+        let l = i+1;
+        let r = n-1;
+        while(l<r){
+        sum = nums[i] + nums[l] + nums[r];
+        if(sum == 0){
+            arr.push([nums[i], nums[l], nums[r]])
+            l++;
+            r--;
+            while(nums[l]==nums[l-1] && l<r) {
+                    l++;
+                }
+        }else if(sum> 0){
+            r--;
+        }
+        else if(sum< 0){
+            l++;
+        }
+        }
+    }
+    return arr
+};
