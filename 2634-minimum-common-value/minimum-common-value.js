58a9ec9f88b1b0a4df7c98bc30920c8f7c04a715
Joanna Taratuta <94808296+Taratutaj@users.noreply.github.com>
2024-03-09 09:11:20 +0100

    Time: 67 ms (78.57%) | Memory: 56.1 MB (91.58%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.js
+++ 2634-minimum-common-value/minimum-common-value.js
@@ -5,15 +5,30 @@
  */
 var getCommon = function(nums1, nums2) {
     let n = nums1.length
+    let m = nums2.length
+    let i1 = 0
+    let i2 = 0
 
-    if(nums1[n-1]<nums2[0]){
-        return -1
+    while(i1< n && i2< m){
+    if(nums1[i1]<nums2[i2]){
+        i1++;
+    }else if(nums1[i1]>nums2[i2]){
+        i2++;
+    }else if(nums1[i1]==nums2[i2]){
+        return nums1[i1]
     }
-
-    for(let i = 0; i<n; i++){
-        if(nums2.includes(nums1[i])){
-            return nums1[i]
-        }
     }
 return -1
 };
+
+
+
+//  if(nums1[n-1]<nums2[0]){
+//         return -1
+//     }
+
+//     for(let i = 0; i<n; i++){
+//         if(nums2.includes(nums1[i])){
+//             return nums1[i]
+//         }
+//     }
