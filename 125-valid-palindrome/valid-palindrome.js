8341decce113ffe79cee641910729cb76fa483e9
Joanna Taratuta <94808296+Taratutaj@users.noreply.github.com>
2024-03-09 11:16:49 +0100

    Time: 50 ms (96.87%) | Memory: 51.9 MB (54.30%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -4,15 +4,15 @@
  */
 var isPalindrome = function(s) {
     let word = s.toLowerCase().replace(/[^a-z0-9]/g, '').split('');
-    console.log(word)
+
 
     n = Math.floor(word.length/2)
-    console.log(n)
+
 
     for(let i =0; i<n; i++){
-        console.log(word[i], word[word.length-1-i])
+
         if(word[i]!=word[word.length-1-i]){
-             console.log("false")
+ 
             return false
            
         }
