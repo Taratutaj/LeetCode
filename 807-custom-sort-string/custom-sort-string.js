0babc804f546d025cd89792e2f0af5f745a5f258
Joanna Taratuta <94808296+Taratutaj@users.noreply.github.com>
2024-03-11 11:20:35 +0100

    Time: 64 ms (12.39%) | Memory: 50.3 MB (31.41%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} order
+ * @param {string} s
+ * @return {string}
+ */
+var customSortString = function(order, s) {
+  //order into arr
+  let arrOrder = order.split("")
+  //s into arr
+  let arrS = s.split("")
+  //create arr
+  let arrSorted = []
+
+  console.log(arrOrder, arrS, arrSorted)
+
+    for(let i= 0; i<arrOrder.length;i++){
+      //take value from order
+
+        //while s has value
+        while(arrS.includes(arrOrder[i])){
+            // add it to arr and delete from s
+            arrSorted.push(arrOrder[i])
+            let idx = arrS.indexOf(arrOrder[i])
+            arrS.splice(idx,1)
+        }
+
+    }
+//at the end push arr into s and turn join
+arrS = arrS.concat(arrSorted)
+let ans = arrS.join("")
+return ans
+
+};
